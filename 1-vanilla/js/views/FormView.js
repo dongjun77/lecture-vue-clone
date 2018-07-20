bd641ef611a117253bb455337b35eee082b8ff1e
dongjun77 <edj0707@naver.com>
2018-07-20 11:44:54 +0900

    second commit
    FormView2 dev
    formview -> MainController -> ResultView as MVC pattern

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -7,7 +7,7 @@ const FormView = Object.create(View)
 FormView.setup = function (el) {
   this.init(el)
   this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
+  this.resetEl = el.querySelector('[type=reset]')
   this.showResetBtn(false)
   this.bindEvents()
   return this
@@ -18,11 +18,18 @@ FormView.showResetBtn = function (show = true) {
 }
 
 FormView.bindEvents = function() {
-    this.inputEl.addEventListener('keyup', e => this.onkeyup(e))
+  this.on('submit', e => e.preventDefault())
+  this.inputEl.addEventListener('keyup', e => this.onkeyup(e))
 }
 
-FormView.onkeyup = function () {
-    this.showResetBtn(this.inputEl.value.length)
+FormView.onkeyup = function (e) {
+  const enter = 13
+  this.showResetBtn(this.inputEl.value.length)
+  if (e.keyCode !== enter) return 
+  //검색결과 View 띄우기
+  this.emit('@submit', { 
+    input: this.inputEl.value 
+  })
 }
 
 export default FormView
