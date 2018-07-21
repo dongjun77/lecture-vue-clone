2439fa697ced8841510eab79d91b595add16bcc2
dongjun77 <edj0707@naver.com>
2018-07-21 15:47:00 +0900

    TabView2 make onChange
    1. add bindClick and queryAll('li')
    2. onClick(tabName) -> emit -> Maincontroller
    3. onChangeTab to debugger (tabName)

--- 1-vanilla/js/views/TabView.js
+++ 1-vanilla/js/views/TabView.js
@@ -6,12 +6,25 @@ const TabView = Object.create(View)
 
 TabView.setup = function (el) {
     this.init(el)
+    this.bindClick()
+    return this
 }
 
 TabView.setActiveTab = function (tabName) {
     Array.from(this.el.querySelectorAll('li')).forEach(li => { //All은 모든 데이터를 반환해주기 때문
         li.className = li.innerHTML === tabName ? 'active' : ''//탭 네임이 같은것 들만 active라는 문자를 주고 아니면 빈 무자열 반환
     })
+}
+
+TabView.bindClick = function () {
+    Array.from(this.el.querySelectorAll('li')).forEach(li => {
+        li.addEventListener('click', e => this.onClick(li.innerHTML))
+    })
+}
+
+TabView.onClick = function (tabName) {
+    this.setActiveTab(tabName) // TAb set 했으니 이후 컨트롤러에 알려줘야한다
+    this.emit('@change', {tabName})
 }
 
 export default TabView // 이래야 컨트롤러에서 사용이 가능하다
