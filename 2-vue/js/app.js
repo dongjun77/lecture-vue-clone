e32a035662493234e0cb075a68e0c7848da289fb
dongjun77 <edj0707@naver.com>
2018-07-22 16:50:20 +0900

    searchResult-add-models
    1. searchmodel.js
    2. v-if, v-for, v-bind

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,16 +1,28 @@
+import SearchModel from './models/SearchModel.js'
 new Vue({
     el: '#app',
     data: {
-        query: ''
+        query: '',
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e) {
-            debugger
+            this.search()
         },
         onKeyup() {
             if(!this.query.length) this.onReset()
         },
-        onReset() {
+        onReset(e) {
+            this.resetForm()
+        },
+        search() {
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
+        },
+        resetForm() {
             this.query = ''//여기서 this는 Vue 의 query를 ''로 만들어준다
             debugger
         }
