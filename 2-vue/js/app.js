bee01e3d2efbbfccac33092ae88788a92ab27c6b
dongjun77 <edj0707@naver.com>
2018-07-22 16:11:09 +0900

    Vue Dom
    1. v-model
    2. v-show
    3. v-on

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,6 +1,14 @@
-new IDBCursorWithValue({
+new Vue({
     el: '#app',
     data: {
-        msg: 'hello world'
+        query: ''
+    },
+    methods: {
+        onSubmit(e) {
+            debugger
+        },
+        onreset() {
+            this.query = ''//여기서 this는 Vue 의 query를 ''로 만들어준다
+        }
     }
 })
